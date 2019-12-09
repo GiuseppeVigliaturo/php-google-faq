d4add93e9f7a411d46a2a6953f14f31a28ab8ca6
giuviglia94 <vigliaturo94@gmail.com>
2019-12-09 17:33:30 +0100

    stampa a schermo

--- index.php
+++ index.php
@@ -2,12 +2,30 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>php-goal</title>
+    <title>php-google-faq</title>
     <link rel="stylesheet" href="style.css">
-  
+    <?php
+    include 'data.php';
+
+     ?>
+
   </head>
   <body>
 
+    <!-- Riscrivere questa pagina del sito google
+    https://policies.google.com/faq.
+    Ci sono diverse domande con relative risposte.
+    Gestire il "Database" e la visualizzazione di
+    queste domande e risposte con PHP. -->
+    <?php
+
+    foreach ($faqs as $key => $value) {
+      echo $value['question']."<br>"."<br>".$value['answer'];
+      echo "<br>";
+
+    }
+
+     ?>
 
 
 
